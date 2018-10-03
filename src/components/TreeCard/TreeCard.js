bce9f9eb522e5f2071a803e9f47c8840e6881436
Steven Penfield <stevenpenfield9@gmail.com>
2018-10-02 22:35:51 -0600

    kicking off a proof of concept for the journal overlay functionality

--- src/components/TreeCard/TreeCard.js
+++ src/components/TreeCard/TreeCard.js
@@ -1,53 +1,40 @@
 import React, { Component } from 'react'
 
 export default class TreeCard extends Component {
-	constructor(){
-		super()
-		this.state={
-			name:"",
-			spouse:"",
-			spouseToggle:false,
-		}
-	}
-
-	spouseToggle(){
-		this.setState({spouseToggle:true})
-	}
-
-	nameHandler(val){
-		this.setState({name:val})
-	}
-
-	spouseHandler(val){
-		this.setState({spouse:val})
-	}
-
-	spouseDelete(){
-		this.setState({spouseToggle:false,spouse:""})
+	constructor(props){
+		super(props)
 	}
 
 	render() {
 		return (
-			<div style={{height:"250px", width:"500px", border:"1px solid red"}}>
-				<div style={{display:"flex"}}>
-					<div style={{display:"flex", width:"50%"}}>
-						<button style={{height:"25px"}}>Delete Person</button><div style={{height:"25px", margin:"5px 10px"}}>{this.state.name}</div>
+			<div className="tree">
+			<div className="tree-card">
+				<div className="button-container">
+					<div className="delete-container">
+						<button>Delete Spouse</button><button>Delete Card</button>	
 					</div>
-					<div style={{display:"flex", flexDirection:"row-reverse", width:"50%"}}>
-						<button style={{height:"25px"}} onClick={()=>this.spouseToggle()}>Add Spouse</button><button style={{height:"25px"}}>Add Child</button>
+					<div className="add-container">
+						<button>Add Spouse</button><button onClick={this.props.addChild}>Add Child</button>	
 					</div>
 				</div>
-				<div>
-					<input placeholder="Your Name" onChange={(e)=>{this.nameHandler(e.target.value)}}></input>
+				<div className="single-person">
+					<div>ID:{this.props.person.id}</div>
+					{this.props.person.name}: {this.props.person.born} - {this.props.person.died}
 				</div>
-				{this.state.spouseToggle ? 
-				<div style={{display:"flex", border:"1px solid blue"}}>
-					<button style={{height:"25px"}} onClick={()=>this.spouseDelete()}>Delete Spouse</button>
-					<div style={{height:"25px", margin:"5px 10px"}}>{this.state.spouse}</div>
-					<input placeholder="Spouses Name" onChange={(e)=>{this.spouseHandler(e.target.value)}}></input>
-				</div> 
-				: null}
+				<div className="single-person">
+					{this.props.person.spouse}: {this.props.person.sborn} - {this.props.person.sdied}
+				</div>
 			</div>
-		)
+				<div className="generation">
+					{this.props.person.children.length 
+					?
+					 this.props.person.children.map((person) => (
+						<TreeCard  person={person} key={person.id}/> 
+					 ))
+					 : null}
+				</div>
+			</div>
+		)   
 	}
 }
+
--- src/components/TreeHolder/TreeHolder.js
+++ src/components/TreeHolder/TreeHolder.js
@@ -5,34 +5,120 @@ export default class TreeHolder extends Component {
 	constructor(){
 		super()
 		this.state={
+			count:1,
 			familyTree:{
-				name:'Bob',
-				spouse:'',
-				children:[
-					{
-						name:'Karen',
-						spouse:'',
+				id:1,
+				name:'Your Name Here',
+				born:1980,
+				died:null,
+				spouse:'Insert Spouse Here',
+				sborn:1980,
+				sdied:null,
+				children:[{
+					id:2,
+					name:'Your Name Here',
+					born:1980,
+					died:null,
+					spouse:'Insert Spouse Here',
+					sborn:1980,
+					sdied:null,
+					children:[]
+				},{
+					id:3,
+					name:'Your Name Here',
+					born:1980,
+					died:null,
+					spouse:'Insert Spouse Here',
+					sborn:1980,
+					sdied:null,
+					children:[]
+				},{
+					id:4,
+					name:'Your Name Here',
+					born:1980,
+					died:null,
+					spouse:'Insert Spouse Here',
+					sborn:1980,
+					sdied:null,
+					children:[{
+						id:5,
+						name:'Your Name Here',
+						born:1980,
+						died:null,
+						spouse:'Insert Spouse Here',
+						sborn:1980,
+						sdied:null,
 						children:[]
-					},
-					{
-						name:'Mike',
-						spouse:'',
+					},{
+						id:6,
+						name:'Your Name Here',
+						born:1980,
+						died:null,
+						spouse:'Insert Spouse Here',
+						sborn:1980,
+						sdied:null,
 						children:[]
-					}
-				]
+					},{
+						id:7,
+						name:'Your Name Here',
+						born:1980,
+						died:null,
+						spouse:'Insert Spouse Here',
+						sborn:1980,
+						sdied:null,
+						children:[]
+					}]
+				}]
 			}
 		}
+	this.addChild = this.addChild.bind(this)
 	}
 
-	treeMaker(){
-		const {familyTree} = this.state
-		return <div><h1>{familyTree.name}</h1><h1>{familyTree.children[0].name}</h1><h1>{familyTree.children[1].name}</h1></div>
+	seeker(arr,findId){
+		return arr.map((obj)=>{
+			if(obj.id === findId){console.log(`got ya b! ${obj.id}`); return this}
+			else{obj.children.length > 0 ? this.seeker(obj.children,findId):null}
+		})
 	}
+
+	personDelete(){
+
+	}
+
+	addChild(identifier){
+		let treeCopy = Object.assign({},this.state.familyTree);
+		let countCopy = this.state.count.valueOf();
+		this.setState({count:countCopy});
+		let gotcha = this.seeker(treeCopy,identifier);
+		console.log(gotcha)
+
+		// find the identifier in the arrays of objects. Use that to point us to the right scope.
+		// then use 'this' to indicate the scope level.
+		// push in a person template object into the children array at 'this' scope.
+	}
+
+	nameChild(){
+		
+	}
+
+	addSpouse(){
+
+	}
+
+	nameSpouse(){
+
+	}
+
+	deleteSpouse(){
+
+	}
+
 
 	render() {
+		let {familyTree} = this.state
 		return (
 			<div>
-				<div>{this.treeMaker()}</div>
+				<TreeCard person={familyTree} addChild={this.addChild}/>
 			</div>
 		)
 	}
